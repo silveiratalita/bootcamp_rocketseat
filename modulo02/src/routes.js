116bb328c6ae69118a1845639a831df9e17c5279
silveiratalita <silveira.tacristina@gmail.com>
2019-08-25 18:52:36 -0300

    Continuidade gobarber-eslint, prettier, sequelize, migration.

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,7 @@
-import  {Router} from 'express';
+import { Router } from "express";
 
-const routes =new Router();
+const routes = new Router();
 
-routes.get('/',(req, res)=>{
-    return res.json({message:"Olá Mundo"});
-})
+routes.get("/", (req, res) => res.json({ message: "Olá Mundo" }));
 
 export default routes;
