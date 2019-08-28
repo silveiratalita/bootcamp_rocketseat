57e0421bbb47f33c4486e7b1e0ce892feac3d960
silveiratalita <silveira.tacristina@gmail.com>
2019-08-27 21:35:48 -0300

    correçaao databasejs

--- modulo02/src/config/database.js
+++ modulo02/src/config/database.js
@@ -1,26 +1,12 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  passwors: "docker",
+  database: "gobarber",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underescoredAll: true,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
-}
+};
